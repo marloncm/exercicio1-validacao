f0b30d5bfd48ce3683c6022b2fb849093183dc0f
Marlon Carvalho Mariano <marlon.cmariano93@gmail.com>
2025-08-30 16:16:40 -0300

    projeto criado

---
+++ src/test/java/ifrs/exercicio1/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package ifrs.exercicio1;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
